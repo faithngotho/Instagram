2e68ba5a0eead6255aca573a2a6e8b81bc64d921
faithngotho <faithwangari248@gmail.com>
2019-07-29 08:42:33 +0300

    add editor to models.py

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
 
-# Create your models here.
+    def __str__(self):
+        return self.first_name
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import datetime as dt
+from django.http import Http404
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to Insta')
+
